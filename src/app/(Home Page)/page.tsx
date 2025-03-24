946fafbb42fc70f82b87f98b46d0e23dbf9992c2
Selvakumar Murugesan <selvaonline@gmail.com>
2025-03-23 23:48:34 -0500

    updated

--- src/app/(Home Page)/page.tsx
+++ src/app/(Home Page)/page.tsx
@@ -5,12 +5,10 @@ import CtaSection from "@/components/cta/CtaSection";
 import FooterSection8 from "@/components/footer/FooterSection8";
 import HeaderSection7 from "@/components/header/HeaderSection7";
 import SearchFormModal from "@/components/modal/SearchFormModal";
-import VideoModal from "@/components/modal/VideoModal";
 import CaseStudySection from "@/components/project/CaseStudySection";
 import ServiceSection6 from "@/components/service/ServiceSection6";
 import StatSection from "@/components/stats/StatSection";
 import TestimonialSection5 from "@/components/testimonial/TestimonialSection5";
-import VideoSection7 from "@/components/video/VideoSection7";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -28,12 +26,10 @@ export default function Home() {
       <CtaSection />
       <CaseStudySection />
       <TestimonialSection5 />
-      <VideoSection7 />
       <BlogSection6 />
       <FooterSection8 />
       {/* Modal */}
       <SearchFormModal />
-      <VideoModal videoUrl="https://www.youtube.com/embed/TboWOSW7qCI?si=Tai9q8LZqyR5R9e6" />
     </main>
   );
 }
